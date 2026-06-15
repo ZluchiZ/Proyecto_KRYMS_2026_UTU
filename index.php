2e1b25bef4932ec7b1691b4e38693e18b1f6a0c6
Godtheguille <gsivoplasecheniqueopo@gmail.com>
2026-06-15 14:54:23 -0300

    Wireframe basico del proyecto

--- index.php
+++ index.php
@@ -4,9 +4,66 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
+   
     <title>ElGauchoVa</title>
 </head>
 <body>
-    <h1>Hola</h1>
+
+<label for="sidebar-toggle" class="menu-btn">☰ Menú</label>
+<input type="checkbox" id="sidebar-toggle">
+
+<div class="top-right-icons">
+    <a href="#" class="icon-btn" title="Ubicación">📍</a>
+    <a href="#" class="icon-btn" title="Perfil">👤​</a>
+</div>
+
+<div class="sidebar">
+    <a href="#">Farmacia</a>
+    <a href="#">Supermercado</a>
+    <a href="#">Ferreteria</a>
+    <a href="#">Rotiseria</a>
+
+</div>
+
+<div class="contenido-pagina">
+<h1>¡Bienvenidos a ElGauchoVa!</h1>
+    <form for="/buscar" method="GET">
+    <label for="campo-busqueda"Buscar:></label>
+    <input type="search" id="campo-busqueda" name="q" placeholder="¿Qué estás buscando?">
+    <button type="submit">buscar</button>
+    </form>
+
+    <div class="comidas-container">
+        <div class="comida-card">
+            <img src="img/comida1.jpg" alt="Comida 1">
+            <h3>Nombre del plato</h3>
+            <p>Descripción del plato.</p>
+        </div>
+        <div class="comida-card">
+            <img src="img/comida2.jpg" alt="Comida 2">
+            <h3>Nombre del plato</h3>
+            <p>Descripción del plato.</p>
+        </div>
+        <div class="comida-card">
+            <img src="img/comida3.jpg" alt="Comida 3">
+            <h3>Nombre del plato</h3>
+            <p>Descripción del plato.</p>
+        </div>
+       
+    </div>
+</div>
+
+<div class="Nosotros">
+    <div class="Sobre-Nosotros">
+        <p><strong>Sobre nosotros</strong></p>
+        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+    </div>
+
+</div>
+
+    
 </body>
 </html>
